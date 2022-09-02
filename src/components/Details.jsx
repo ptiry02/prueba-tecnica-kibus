fd80993e7b0ddf95c9dfd0bb600fe9548908a6b8
Paul Tiry <tirypaul@gmail.com>
2022-09-02 22:02:43 +0200

    made foto slider

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,26 +1,65 @@
 import styled from 'styled-components'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import Recipes from './Recipes'
 
 const Details = () => {
   return (
-    <Testing>
+    <Wrapper>
       <Container>
         <Recipes />
       </Container>
-    </Testing>
+      <SlideBackground>
+        <Swiper
+          pagination={{
+            clickable: true
+          }}
+          centeredSlides={true}
+          slidesPerView={1.2}
+        >
+          <SwiperSlide>
+            <Img src='assets/images/043-kibus.jpeg' alt='kibus' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src='assets/images/043-kibus.jpeg' alt='kibus' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src='assets/images/043-kibus.jpeg' alt='kibus' />
+          </SwiperSlide>
+        </Swiper>
+      </SlideBackground>
+    </Wrapper>
   )
 }
 
 export default Details
 
-const Testing = styled.div`
+const Wrapper = styled.div`
+  position: relative;
   width: 100%;
-  height: 500px;
   background-color: ${({ theme }) => theme.colors.secBack};
+  background-image: url('assets/images/bgIcons.png');
+  background-size: contain;
+  display: flex;
+  flex-direction: column;
 `
 const Container = styled.div`
+  position: relative;
+  top: -140px;
   overflow-x: auto;
-  padding: 40px 6.5% 40px 6.5%;
-  transform: translateY(-120px);
+  padding: 30px 6.5% 60px 6.5%;
+`
+const SlideBackground = styled.div`
+  position: absolute;
+  top: 370px;
+  width: 100%;
+  background-image: url('assets/images/Ellipse_8.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  padding: 25px 0;
+`
+const Img = styled.img`
+  width: 100%;
+  border-radius: 15px;
 `
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'swiper/css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
